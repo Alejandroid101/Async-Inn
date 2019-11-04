a72ae6c9edcb7494c39d5dd1cd922a3d1ef46925
Alejandroid101 <kik.rdl3@gmail.com>
2019-11-03 21:46:58 -0800

    having aN ERROR while trying to start database

---
+++ AsyncHotels/AsyncHotels/Data/AsyncDbContext.cs
@@ -0,0 +1,23 @@
+﻿using AsyncHotels.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncHotels.Data
+{
+    public class AsyncDbContext : DbContext
+    {
+        public AsyncDbContext(DbContextOptions<AsyncDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<HotelRoom> HotelRooms { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomAmenities> RoomAmenities { get; set; }
+        public DbSet<Amenities> Amenities { get; set; }
+    }
+}
